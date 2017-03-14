0905dd042d3885628b961b5b1be716a61e22021b
correy <cwinke3@gmail.com>
2017-03-14 16:25:52 -0500

    Jared's work

---
+++ stage2/Wisconsin/highlight.js
@@ -0,0 +1,295 @@
+	function blankWisconsin() {
+  document.getElementById("wisconsinmap").src="cnty.gif";
+  document.getElementById("searchtitle").innerHTML = "<h2>Search by county</h2>";
+  document.getElementById("countyName").innerHTML = "<h2>Please select a county to browse</h2>"; 
+  
+  }  
+	function greenCounty() {
+  document.getElementById("wisconsinmap").src="counties/green.gif"; 
+	document.getElementById("countyName").innerHTML = "Green County, Wisconsin"; 
+  }
+  
+	function adamsCounty() {
+  document.getElementById("wisconsinmap").src="counties/adams.gif";  
+  document.getElementById("countyName").innerHTML = "Adams County, Wisconsin"; 
+  }
+    function ashlandCounty() {
+  document.getElementById("wisconsinmap").src="counties/ashland.gif";  
+  document.getElementById("countyName").innerHTML = "Ashland County, Wisconsin"; 
+  }
+    function barronCounty() {
+  document.getElementById("wisconsinmap").src="counties/barron.gif"; 
+document.getElementById("countyName").innerHTML = "Barron County, Wisconsin";   
+  }
+    function bayfieldCounty() {
+  document.getElementById("wisconsinmap").src="counties/bayfield.gif";  
+  document.getElementById("countyName").innerHTML = "Bayfield County, Wisconsin"; 
+  }
+    function brownCounty() {
+  document.getElementById("wisconsinmap").src="counties/brown.gif";  
+  document.getElementById("countyName").innerHTML = "Brown County, Wisconsin"; 
+  }
+    function buffaloCounty() {
+  document.getElementById("wisconsinmap").src="counties/buffalo.gif";  
+  document.getElementById("countyName").innerHTML = "Buffalo County, Wisconsin"; 
+  }
+    function burnettCounty() {
+  document.getElementById("wisconsinmap").src="counties/burnett.gif";  
+  document.getElementById("countyName").innerHTML = "Burnett County, Wisconsin"; 
+  }
+    function calumetCounty() {
+  document.getElementById("wisconsinmap").src="counties/calumet.gif";  
+  document.getElementById("countyName").innerHTML = "Calumet County, Wisconsin"; 
+  }
+    function chippewaCounty() {
+  document.getElementById("wisconsinmap").src="counties/chippewa.gif";  
+  document.getElementById("countyName").innerHTML = "Chippewa County, Wisconsin"; 
+  }
+    function clarkCounty() {
+  document.getElementById("wisconsinmap").src="counties/clark.gif";  
+  document.getElementById("countyName").innerHTML = "Clark County, Wisconsin"; 
+  }
+    function columbiaCounty() {
+  document.getElementById("wisconsinmap").src="counties/columbia.gif";  
+  document.getElementById("countyName").innerHTML = "Columbia County, Wisconsin"; 
+  }
+    function crawfordCounty() {
+  document.getElementById("wisconsinmap").src="counties/crawford.gif";  
+  document.getElementById("countyName").innerHTML = "Crawford County, Wisconsin"; 
+  }
+    function daneCounty() {
+  document.getElementById("wisconsinmap").src="counties/dane.gif";  
+  document.getElementById("countyName").innerHTML = "Dane County, Wisconsin"; 
+  }
+    function dodgeCounty() {
+  document.getElementById("wisconsinmap").src="counties/dodge.gif";  
+  document.getElementById("countyName").innerHTML = "Dodge County, Wisconsin"; 
+  }
+    function doorCounty() {
+  document.getElementById("wisconsinmap").src="counties/door.gif";  
+  document.getElementById("countyName").innerHTML = "Door County, Wisconsin"; 
+  }
+    function douglasCounty() {
+  document.getElementById("wisconsinmap").src="counties/douglas.gif";  
+  document.getElementById("countyName").innerHTML = "Douglas County, Wisconsin"; 
+  }
+    function dunnCounty() {
+  document.getElementById("wisconsinmap").src="counties/dunn.gif";  
+  document.getElementById("countyName").innerHTML = "Dunn County, Wisconsin"; 
+  }
+    function eauclaireCounty() {
+  document.getElementById("wisconsinmap").src="counties/eauclaire.gif";  
+  document.getElementById("countyName").innerHTML = "Eau Claire County, Wisconsin"; 
+  }
+    function florenceCounty() {
+  document.getElementById("wisconsinmap").src="counties/florence.gif";  
+  document.getElementById("countyName").innerHTML = "Florence County, Wisconsin"; 
+  }
+    function fonddulacCounty() {
+  document.getElementById("wisconsinmap").src="counties/fonddulac.gif";  
+  document.getElementById("countyName").innerHTML = "Fond du Lac County, Wisconsin"; 
+  }
+    function forestCounty() {
+  document.getElementById("wisconsinmap").src="counties/forest.gif";  
+  document.getElementById("countyName").innerHTML = "Forest County, Wisconsin"; 
+  }
+    function grantCounty() {
+  document.getElementById("wisconsinmap").src="counties/grant.gif"; 
+document.getElementById("countyName").innerHTML = "Grant County, Wisconsin";   
+  }
+    function greenlakeCounty() {
+  document.getElementById("wisconsinmap").src="counties/greenlake.gif";  
+  document.getElementById("countyName").innerHTML = "Green Lake County, Wisconsin"; 
+  }
+    function iowaCounty() {
+  document.getElementById("wisconsinmap").src="counties/iowa.gif";  
+  document.getElementById("countyName").innerHTML = "Iowa County, Wisconsin"; 
+  }
+    function ironCounty() {
+  document.getElementById("wisconsinmap").src="counties/iron.gif";  
+  document.getElementById("countyName").innerHTML = "Iron County, Wisconsin"; 
+  }
+    function jacksonCounty() {
+  document.getElementById("wisconsinmap").src="counties/jackson.gif";  
+  document.getElementById("countyName").innerHTML = "Jackson County, Wisconsin"; 
+  }
+    function jeffersonCounty() {
+  document.getElementById("wisconsinmap").src="counties/jefferson.gif";  
+  document.getElementById("countyName").innerHTML = "Jefferson County, Wisconsin"; 
+  }
+    function juneauCounty() {
+  document.getElementById("wisconsinmap").src="counties/juneau.gif";  
+  document.getElementById("countyName").innerHTML = "Juneau County, Wisconsin"; 
+  }
+    function kenoshaCounty() {
+  document.getElementById("wisconsinmap").src="counties/kenosha.gif";  
+  document.getElementById("countyName").innerHTML = "Kenosha County, Wisconsin"; 
+  }
+    function kewauneeCounty() {
+  document.getElementById("wisconsinmap").src="counties/kewaunee.gif";  
+  document.getElementById("countyName").innerHTML = "Kewaunee County, Wisconsin"; 
+  }
+    function lacrosseCounty() {
+  document.getElementById("wisconsinmap").src="counties/lacrosse.gif";  
+  document.getElementById("countyName").innerHTML = "La Crosse County, Wisconsin"; 
+  }
+    function lafayetteCounty() {
+  document.getElementById("wisconsinmap").src="counties/lafayette.gif";  
+  document.getElementById("countyName").innerHTML = "Lafayette County, Wisconsin"; 
+  }
+    function langladeCounty() {
+  document.getElementById("wisconsinmap").src="counties/langlade.gif";  
+  document.getElementById("countyName").innerHTML = "Langlade County, Wisconsin"; 
+  }
+    function lincolnCounty() {
+  document.getElementById("wisconsinmap").src="counties/lincoln.gif";  
+  document.getElementById("countyName").innerHTML = "Lincoln County, Wisconsin"; 
+  }
+    function manitowocCounty() {
+  document.getElementById("wisconsinmap").src="counties/manitowoc.gif";  
+  document.getElementById("countyName").innerHTML = "Manitowoc County, Wisconsin"; 
+  }
+    function marathonCounty() {
+  document.getElementById("wisconsinmap").src="counties/marathon.gif";  
+  document.getElementById("countyName").innerHTML = "Marathon County, Wisconsin"; 
+  }
+    function marinetteCounty() {
+  document.getElementById("wisconsinmap").src="counties/marinette.gif";  
+  document.getElementById("countyName").innerHTML = "Marinette County, Wisconsin"; 
+  }
+    function marquetteCounty() {
+  document.getElementById("wisconsinmap").src="counties/marquette.gif";  
+  document.getElementById("countyName").innerHTML = "Marquette County, Wisconsin"; 
+  }
+    function menomineeCounty() {
+  document.getElementById("wisconsinmap").src="counties/menominee.gif";  
+  document.getElementById("countyName").innerHTML = "Menominee County, Wisconsin"; 
+  }
+    function milwaukeeCounty() {
+  document.getElementById("wisconsinmap").src="counties/milwaukee.gif";  
+  document.getElementById("countyName").innerHTML = "Milwaukee County, Wisconsin"; 
+  }
+    function monroeCounty() {
+  document.getElementById("wisconsinmap").src="counties/monroe.gif"; 
+document.getElementById("countyName").innerHTML = "Monroe County, Wisconsin";   
+  }
+    function ocontoCounty() {
+  document.getElementById("wisconsinmap").src="counties/oconto.gif";  
+  document.getElementById("countyName").innerHTML = "Oconto County, Wisconsin"; 
+  }
+    function oneidaCounty() {
+  document.getElementById("wisconsinmap").src="counties/oneida.gif";  
+  document.getElementById("countyName").innerHTML = "Oneida County, Wisconsin"; 
+  }
+    function outagamieCounty() {
+  document.getElementById("wisconsinmap").src="counties/outagamie.gif";  
+  document.getElementById("countyName").innerHTML = "Outagamie County, Wisconsin"; 
+  }
+    function ozaukeeCounty() {
+  document.getElementById("wisconsinmap").src="counties/ozaukee.gif";  
+  document.getElementById("countyName").innerHTML = "Ozaukee County, Wisconsin"; 
+  }
+    function pepinCounty() {
+  document.getElementById("wisconsinmap").src="counties/pepin.gif";  
+  document.getElementById("countyName").innerHTML = "Pepin County, Wisconsin"; 
+  }
+    function pierceCounty() {
+  document.getElementById("wisconsinmap").src="counties/pierce.gif";  
+  document.getElementById("countyName").innerHTML = "Pierce County, Wisconsin"; 
+  }
+    function polkCounty() {
+  document.getElementById("wisconsinmap").src="counties/polk.gif";  
+  document.getElementById("countyName").innerHTML = "Polk County, Wisconsin"; 
+  }
+    function portageCounty() {
+  document.getElementById("wisconsinmap").src="counties/portage.gif";  
+  document.getElementById("countyName").innerHTML = "Portage County, Wisconsin"; 
+  }
+    function priceCounty() {
+  document.getElementById("wisconsinmap").src="counties/price.gif";  
+  document.getElementById("countyName").innerHTML = "Price County, Wisconsin"; 
+  }
+    function racineCounty() {
+  document.getElementById("wisconsinmap").src="counties/racine.gif"; 
+document.getElementById("countyName").innerHTML = "Racine County, Wisconsin";   
+  }
+    function richlandCounty() {
+  document.getElementById("wisconsinmap").src="counties/richland.gif";  
+  document.getElementById("countyName").innerHTML = "Richland County, Wisconsin"; 
+  }
+    function rockCounty() {
+  document.getElementById("wisconsinmap").src="counties/rock.gif";  
+  document.getElementById("countyName").innerHTML = "Rock County, Wisconsin"; 
+  }
+    function ruskCounty() {
+  document.getElementById("wisconsinmap").src="counties/rusk.gif";  
+  document.getElementById("countyName").innerHTML = "Rusk County, Wisconsin"; 
+  }
+    function saintcroixCounty() {
+  document.getElementById("wisconsinmap").src="counties/saintcroix.gif";  
+  document.getElementById("countyName").innerHTML = "St. Croix County, Wisconsin"; 
+  }
+    function saukCounty() {
+  document.getElementById("wisconsinmap").src="counties/sauk.gif"; 
+document.getElementById("countyName").innerHTML = "Sauk County, Wisconsin";   
+  }
+    function sawyerCounty() {
+  document.getElementById("wisconsinmap").src="counties/sawyer.gif";  
+  document.getElementById("countyName").innerHTML = "Sawyer County, Wisconsin"; 
+  }
+      function shawanoCounty() {
+  document.getElementById("wisconsinmap").src="counties/shawana.gif";  
+  document.getElementById("countyName").innerHTML = "Shawano County, Wisconsin"; 
+  }
+    function sheboyganCounty() {
+  document.getElementById("wisconsinmap").src="counties/sheboygan.gif";  
+  document.getElementById("countyName").innerHTML = "Sheboygan County, Wisconsin"; 
+  }
+    function taylorCounty() {
+  document.getElementById("wisconsinmap").src="counties/taylor.gif";  
+  document.getElementById("countyName").innerHTML = "Taylor County, Wisconsin"; 
+  }
+    function trempealeauCounty() {
+  document.getElementById("wisconsinmap").src="counties/trempealeau.gif";  
+  document.getElementById("countyName").innerHTML = "Trempealeau County, Wisconsin"; 
+  }
+    function vernonCounty() {
+  document.getElementById("wisconsinmap").src="counties/vernon.gif";  
+  document.getElementById("countyName").innerHTML = "Vernon County, Wisconsin"; 
+  }
+    function vilasCounty() {
+  document.getElementById("wisconsinmap").src="counties/vilas.gif";  
+  document.getElementById("countyName").innerHTML = "Vilas County, Wisconsin"; 
+  }
+    function walworthCounty() {
+  document.getElementById("wisconsinmap").src="counties/walworth.gif";  
+  document.getElementById("countyName").innerHTML = "Walworth County, Wisconsin"; 
+  }
+    function washburnCounty() {
+  document.getElementById("wisconsinmap").src="counties/washburn.gif";  
+  document.getElementById("countyName").innerHTML = "Washburn County, Wisconsin"; 
+  }
+    function washingtonCounty() {
+  document.getElementById("wisconsinmap").src="counties/washington.gif"; 
+document.getElementById("countyName").innerHTML = "Washington County, Wisconsin";   
+  }
+    function waukeshaCounty() {
+  document.getElementById("wisconsinmap").src="counties/waukesha.gif"; 
+document.getElementById("countyName").innerHTML = "Waukesha County, Wisconsin";   
+  }
+    function waupacaCounty() {
+  document.getElementById("wisconsinmap").src="counties/waupaca.gif";  
+  document.getElementById("countyName").innerHTML = "Waupaca County, Wisconsin"; 
+  }
+    function wausharaCounty() {
+  document.getElementById("wisconsinmap").src="counties/waushara.gif";  
+  document.getElementById("countyName").innerHTML = "Waushara County, Wisconsin"; 
+  }
+    function winnebagoCounty() {
+  document.getElementById("wisconsinmap").src="counties/winnebago.gif"; 
+document.getElementById("countyName").innerHTML = "Winnebago County, Wisconsin";   
+  }
+    function woodCounty() {
+  document.getElementById("wisconsinmap").src="counties/wood.gif";  
+  document.getElementById("countyName").innerHTML = "Wood County, Wisconsin"; 
+  }
